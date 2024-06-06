55299f6eab88842ef53da8e906961f2332bffe81
AndyHou <andyhou965@gmail.com>
2024-06-06 12:22:56 +0400

    update the Testimonials cards

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -8,11 +8,13 @@ const Clients = () => {
         Kind words from <span className="text-purple">satisfied clients</span>
       </h1>
       <div className="flex flex-col items-center max-lg:mt-10">
-        <InfiniteMovingCards
-          items={testimonials}
-          direction="right"
-          speed="slow"
-        />
+        <div className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased  items-center justify-center relative overflow-hidden">
+          <InfiniteMovingCards
+            items={testimonials}
+            direction="right"
+            speed="slow"
+          />
+        </div>
         <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
           {companies.map(({ id, img, name, nameImg }) => (
             <div key={id} className="flex md:max-w-60 max-w-32 gap-2">
