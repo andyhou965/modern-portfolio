ea2c72e7cd15b87f35a013010282fdfe647a2e9d
AndyHou <andyhou965@gmail.com>
2024-06-06 12:07:57 +0400

    finish the Testimonials section

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Grid from "@/components/Grid";
 import { FaHome } from "react-icons/fa";
 import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
+import Clients from "@/components/Clients";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProjects />
+        <Clients />
       </div>
     </main>
   );
